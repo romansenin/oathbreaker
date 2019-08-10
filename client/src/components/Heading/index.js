35c02b17515398ff2cea20c6f442e5ef2253c2e9
Roman <romansenin15@gmail.com>
2019-08-10 12:45:03 -0700

    started login page

---
+++ client/src/components/Heading/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Icon from "../../components/Icon";
+import Title from "../../components/Title";
+
+const Heading = () => {
+  return (
+    <div className="heading-container">
+      <Icon />
+      <Title />
+    </div>
+  );
+};
+
+export default Heading;
