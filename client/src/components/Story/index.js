3ad9939f40b29d5002d78d82f4b53af28616e1b2
Phillip Booker <phillippbooker@gmail.com>
2019-08-10 13:17:38 -0700

    Made basic components for landing page.

---
+++ client/src/components/Story/index.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import './style.css';
+
+const Story = () => {
+    return (
+      <>
+      <h2>This is the story component</h2>
+      </>
+    );
+  };
+  
+export default Story;
--- client/src/pages/LandingPage/index.js
+++ client/src/pages/LandingPage/index.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import Hero from '../../components/Hero';
+import Quote from '../../components/Quote';
+import Story from '../../components/Story';
 
 export default class LandingPage extends Component {
   constructor(props) {
@@ -6,6 +9,17 @@ export default class LandingPage extends Component {
     this.state = {};
   }
   render() {
-    return <h1>LandingPage</h1>;
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <h1>LandingPage</h1>
+            <Hero></Hero>
+            <Quote></Quote>
+            <Story></Story>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
