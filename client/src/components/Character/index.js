ec84737e5df8f9b359f9b6d7ede58d7d872c6ebf
paigesmith3794 <paigesmith3794@gmail.com>
2019-08-15 09:30:29 -0700

    animation added to character cards, background added, font for title added

--- client/src/components/Character/index.js
+++ client/src/components/Character/index.js
@@ -1,27 +1,26 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './style.css';
 
-class Character extends Component{
+class Character extends Component {
     state = {
         ids: [1, 2, 3, 4]
     };
 
-    render(){
+    render() {
         return (
-            <div className="char-box">
-                <p>Name: {this.props.name}</p>
-                <div className="img-container">
-                    <img
-                        alt={this.props.name}
-                        src={this.props.image}
-                    />
+            <div className="character-img-container">
+                <img
+                    alt={this.props.name}
+                    src={this.props.image}
+                />
+                <div className="character-char-box">
+                    <p>{this.props.name}</p>
+                    <h3>Stats:</h3>
+                    <p>Health: {this.props.health}</p>
+                    <p>Attack: {this.props.attack}</p>
+                    <p>Defense: {this.props.defense}</p>
+                    <p>Agility: {this.props.agility}</p>
                 </div>
-                <p>Health: {this.props.health}</p>
-                <p>Attack: {this.props.attack}</p>
-                <p>Defense: {this.props.defense}</p>
-                <p>Agility: {this.props.agility}</p>
-                {this.props.handleAttack ? <button onClick={() => this.props.handleAttack(this.props.id, this.props.target)}>Attack</button> : ""}
-                
             </div>
         );
     }
