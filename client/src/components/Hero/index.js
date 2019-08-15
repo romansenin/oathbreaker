c6bb7039d363a441e07f5e7885b7f6b484ee8304
Loren Brown <viveleloren@gmail.com>
2019-08-14 20:13:12 -0700

    Introduction of Fog Animation

--- client/src/components/Hero/index.js
+++ client/src/components/Hero/index.js
@@ -1,14 +1,21 @@
-import React, {Component} from 'react';
-import './style.css';
+import React, { Component } from "react";
+import "./style.css";
 
-const Hero = () => {
-    return (
-      <>
-      <div id="hero-box" className="text-center">
-        <h2>This is the hero component</h2>
-      </div>
-      </>
-    );
-  };
-  
-export default Hero;
+const Fog = () => {
+  return (
+    <div className="hero-wrapper">
+      <section class="fog">
+        <div class="absolute-bg" />
+        <div class="fog-container" />
+        <div class="fog-img fog-img-first" />
+        <div class="fog-img fog-img-first" />
+
+        <div class="title">
+          <h1>OathBreaker</h1>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Fog;
