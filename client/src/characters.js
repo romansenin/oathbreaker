fcb6032748aac96373f7e8559d76d48c16541edf
paigesmith3794 <paigesmith3794@gmail.com>
2019-08-20 13:26:15 -0700

    character.js file created, still working on annimation for stats, images rendered to character page

---
+++ client/src/characters.js
@@ -0,0 +1,107 @@
+const characters = [
+
+    {
+        "id": 1,
+        "name": "Deadeye",
+        "image": require("./images/deadeye.jpg"),
+        "faction": 0,
+        "health": 100,
+        "attack": 15,
+        "defense": 5,
+        "agility": 25
+    },
+    {
+        "id": 2,
+        "name": "Nexus",
+        "image": require("./images/nexus.jpg"),
+        "faction": 0,
+        "health": 200,
+        "attack": 10,
+        "defense": 10,
+        "agility": 5
+    },
+    {
+        "id": 3,
+        "name": "Vanquisher",
+        "image": require("./images/vanquisher.jpg"),
+        "faction": 0,
+        "health": 100,
+        "attack": 15,
+        "defense": 5,
+        "agility": 25
+    },
+    {
+        "id": 4,
+        "name": "Myrmidon",
+        "image": require("./images/myrmidon.jpg"),
+        "faction": 0,
+        "health": 100,
+        "attack": 15,
+        "defense": 5,
+        "agility": 25
+    },
+    {
+        "id": 5,
+        "name": "Outlaw",
+        "image": require("./images/outlaw.jpg"),
+        "faction": 0,
+        "health": 100,
+        "attack": 15,
+        "defense": 5,
+        "agility": 25
+    },
+    {
+        "id": 6,
+        "name": "Duelist",
+        "image": require("./images/duelist.jpg"),
+        "faction": 1,
+        "health": 100,
+        "attack": 15,
+        "defense": 5,
+        "agility": 25
+    },
+    {
+        "id": 7,
+        "name": "Sentinal",
+        "image": require("./images/sentinal.jpg"),
+        "faction": 1,
+        "health": 200,
+        "attack": 10,
+        "defense": 10,
+        "agility": 5
+    },
+    {
+        "id": 8,
+        "name": "Templar",
+        "image": require("./images/templar.jpg"),
+        "faction": 1,
+        "health": 100,
+        "attack": 15,
+        "defense": 5,
+        "agility": 25
+    },
+    {
+        "id": 9,
+        "name": "Paladin",
+        "image": require("./images/paladin.jpg"),
+        "faction": 1,
+        "health": 100,
+        "attack": 15,
+        "defense": 5,
+        "agility": 25
+    },
+    {
+        "id": 10,
+        "name": "Songbow",
+        "image": require("./images/songbow.jpg"),
+        "faction": 1,
+        "health": 100,
+        "attack": 15,
+        "defense": 5,
+        "agility": 25
+    }
+
+
+]
+
+module.exports = characters;
