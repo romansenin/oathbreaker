f0ac10d9599b08e55b88878528f6e792fc0f4137
Roman <romansenin15@gmail.com>
2019-08-23 23:30:17 -0700

    added transparent navbar

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,36 +1,85 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
 
-const Navbar = () => {
+import "./style.css";
+
+const Navbar = props => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <Link to="/" className="navbar-brand">Game </Link>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <Link to="/" className="navbar-brand">
+        OathBreaker
+      </Link>
 
       <div className="button-wrapper">
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
         </button>
       </div>
-
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item">
-            <NavLink exact to="/" className="nav-link" activeClassName="active">Home</NavLink>
+        <ul className="navbar-nav ml-auto">
+          {props.view === "character" ? (
+            <li className="nav-item hide-desktop">
+              <NavLink
+                exact
+                to="/allegiance"
+                className="nav-link"
+                activeClassName="active"
+              >
+                Choose Allegiance
+              </NavLink>
+            </li>
+          ) : (
+            ""
+          )}
+
+          <li className="nav-item hide-desktop">
+            <NavLink
+              exact
+              to="/auth/logout"
+              className="nav-link"
+              activeClassName="active"
+            >
+              Logout
+            </NavLink>
           </li>
-          <li className="nav-item">
-            <NavLink exact to="/login" className="nav-link" activeClassName="active">Login</NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink exact to="/signup" className="nav-link" activeClassName="active">Sign Up</NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink exact to="/allegiance" className="nav-link" activeClassName="active">Choose Allegiance</NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink exact to="/character" className="nav-link" activeClassName="active">Choose Character</NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink exact to="/battle" className="nav-link" activeClassName="active">Battle</NavLink>
+          <li className="nav-item dropdown">
+            <Link
+              to="/"
+              className="nav-link dropdown-toggle"
+              id="navbarDropdown"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              {props.user.displayName}
+            </Link>
+            <div
+              className="dropdown-menu dropdown-menu-right"
+              aria-labelledby="navbarDropdown"
+            >
+              {props.view === "character" ? (
+                <>
+                  <Link className="dropdown-item" to="/allegiance">
+                    Choose Allegiance
+                  </Link>
+                  <div className="dropdown-divider" />
+                </>
+              ) : (
+                ""
+              )}
+
+              <a className="dropdown-item" href="/auth/logout">
+                Logout
+              </a>
+            </div>
           </li>
         </ul>
       </div>
